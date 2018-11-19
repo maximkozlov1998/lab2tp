72973ddf11c95fd85bf4802c46f85a5c39a8ee32
kozlov421998 <kozlov421998@gmail.com>
2018-11-19 13:18:04 +0800

    Добавьте файлы проекта.

---
+++ lab2tp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2tp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            do
+            {
+                try
+                {
+                    string path; //путь 
+                    Console.WriteLine("Введите путь к папке"); // C:\...\название файла.txt 
+                    path = Console.ReadLine();
+
+                    int code; //кодировка 
+                    Console.WriteLine("Введите кодировку:Win(1251),DOC(866),UTF-8(65001)");
+                    code = int.Parse(Console.ReadLine());
+
+                    using (TextReader txt = new StreamReader(path, Encoding.GetEncoding(code)))
+                    {
+                        Console.WriteLine(txt.ReadToEnd());
+                    }
+                    flag = true;
+                }
+                catch (Exception)
+                { }
+            }
+            while (!flag);
+
+            Console.ReadKey();
+        }
+    }
+}
